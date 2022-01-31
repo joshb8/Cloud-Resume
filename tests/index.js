f5e3f9cfcee49df3df1256d3389ea696ba7953a8
Joshua Bernier <joshua.bernier8@gmail.com>
2022-01-31 18:44:38 -0500

    first commit

---
+++ tests/index.js
@@ -0,0 +1,24 @@
+const puppeteer = require('puppeteer');
+const { DOMAIN_NAME } = "joshscloudresume.com";
+
+const sleep = async (r) => await new Promise(r => setTimeout(r, 2000));
+
+(async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    const website = `http://joshscloudresume`
+    console.log(`Loading: ${website}`)
+    await page.goto(website)
+    console.log(`Waiting for API calls to be made`)
+    await sleep(2000)
+    const element = await page.$("#body")
+    const property = await element.getProperty('innerHTML');
+    const count = await property.jsonValue();
+    console.log(`Getting page element, count: ${data}`)
+    if (!count) {
+        throw new Error("Cannot find count value")
+    } else {
+        console.log("PASS");
+    }
+    await browser.close();
+})();
