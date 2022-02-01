6e32cce045845c23bfc257236b1ffcc15ee95907
Joshua Bernier <joshua.bernier8@gmail.com>
2022-01-31 19:00:41 -0500

    first commit

--- tests/end-to-end-test/index.js
+++ tests/end-to-end-test/index.js
@@ -1,12 +1,12 @@
 const puppeteer = require('puppeteer');
-const { DOMAIN_NAME } = "joshscloudresume.com";
+const { DOMAIN_NAME } = "joshscloudresume.net";
 
 const sleep = async (r) => await new Promise(r => setTimeout(r, 2000));
 
 (async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    const website = `http://joshscloudresume`
+    const website = `http://joshscloudresume.net`
     console.log(`Loading: ${website}`)
     await page.goto(website)
     console.log(`Waiting for API calls to be made`)
